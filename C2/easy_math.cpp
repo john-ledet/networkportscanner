ebff8f8e0039817d0f9d22c6a38d1ceb356583c7
Blake Dejohn <unknowingly@tamu.edu>
2024-11-01 11:26:33 -0500

    added a question and cleared the terminal more often

--- C2/easy_math.cpp
+++ C2/easy_math.cpp
@@ -190,10 +190,14 @@ bool ask_question(const std::string& question, const std::string& answer) {
 
     //user didn't answer in time
     if(timeout && user_answer.empty()) {
+        //clear the terminal if they run out of time
+        clear_terminal();
         std::cout << "Time's up!" << std::endl;
         return false;
     }
     if(!correct){
+        //clear the terminal if they get the question wrong
+        clear_terminal();
         std::cout << "That was incorrect!" << std::endl;
     }
 
@@ -201,6 +205,7 @@ bool ask_question(const std::string& question, const std::string& answer) {
 }
 
 int main(){
+    const int NUMBER_OF_QUESTIONS = 4;
     std::cout << "Make sure you have libcurl installed on your linux machine" << std::endl;
     if (isWSL()) {
         std::remove("easy_math");
@@ -209,19 +214,21 @@ int main(){
 
     std::signal(SIGALRM, signal_handler);
 
-    std::string questions[3] = {
+    std::string questions[NUMBER_OF_QUESTIONS] = {
         "\n\nWhat is the log(100)th derivative of h(x) = 3x^7 - 6x^4 + 8x^3 -12x + 18?\n\n (a) 21x^6 - 24x^3 +24x^2 - 12\n (b) 21x^6 - 12x^3 + 12x - 12\n (c) 126x^5 - 72x^2 - 48x\n (d) 126x^5 - 72x^2 + 48x\n",
         "\n\nWhat is the 56th derivative of sin(x)?\n\n (a) sin(x)\n (b) cos(x)\n (c) -sin(x)\n (d) -cos(x)\n",
-        "\n\nWhat is the integral of 1 / (sqrt(x) * (x + 1))\n\n (a) cos^-1(x) + C\n (b) 2sin^-1(1/x) + C\n (c) 2tan^-1(sqrt(x)) + C\n (d) 2* sqrt(x) + 2 * ln(x - 1) + C\n"
+        "\n\nWhat is the integral of 1 / (sqrt(x) * (x + 1))\n\n (a) cos^-1(x) + C\n (b) 2sin^-1(1/x) + C\n (c) 2tan^-1(sqrt(x)) + C\n (d) 2* sqrt(x) + 2 * ln(x - 1) + C\n",
+        "\n\nWhat is the integral of 1 / (x^2 + 1)\n\n (a) tan^-1(x) + C\n (b) sin^-1(x) + C\n (c) cos^-1(x) + C\n (d) -tan^-1(x) + C\n",
     };
 
-    std::string answers[3] = {
+    std::string answers[NUMBER_OF_QUESTIONS] = {
         "d",
         "a",
-        "c"
+        "c",
+        "a",
     };
 
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < NUMBER_OF_QUESTIONS; i++) {
         if(!ask_question(questions[i], answers[i])) {
             std::remove("easy_math");
             return 1;
